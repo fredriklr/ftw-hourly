d60cac6b0ad6e8ade06a2c0386454eb620c98a96
Magnus Lauvli Andersen <magnuslauvliandersen@Magnuss-MBP-2.fritz.box>
2021-04-18 13:59:20 +0200

    Lagt inn enkel HTML text på FAQ siden

--- src/containers/FAQPage/FAQPage.js
+++ src/containers/FAQPage/FAQPage.js
@@ -23,9 +23,34 @@ const FAQPage = () => {
           <h1>Frequently Asked Questions</h1>
 
           <div>
+            <h2> For utleier</h2>
+
             <h3>Question 1?</h3>
             <p>Answer: Lorem ipsum</p>
+            <h3>Question 2?</h3>
+            <p>Answer: Lorem ipsum</p>
+            <h3>Question 3?</h3>
+            <p>Answer: Lorem ipsum</p>
+            <h3>Question 4?</h3>
+            <p>Answer: Lorem ipsum</p>
           </div>
+
+           <div>
+            <h2>For leier</h2>
+
+            <h3>Question 1?</h3>
+            <p>Answer: Lorem ipsum</p>
+            <h3>Question 2?</h3>
+            <p>Answer: Lorem ipsum</p>
+            <h3>Question 3?</h3>
+            <p>Answer: Lorem ipsum</p>
+            <h3>Question 4?</h3>
+            <p>Answer: Lorem ipsum</p>
+          </div>
+
+
+
+
         </LayoutWrapperMain>
 
         <LayoutWrapperFooter>
